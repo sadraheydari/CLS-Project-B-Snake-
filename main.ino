1ba3d7e2bfbb94ac33d4e6b24357e02a7b12b162
sadraheydari <msadrah521@gmail.com>
2020-01-24 18:00:00 +0330

    create main file and define GV

---
+++ main.ino
@@ -0,0 +1,27 @@
+// Boolean[8][8]    game
+// int              x_dir   |-1 -> left| +1 -> right|
+// int              y_dir   |-1 -> down| +1 -> Up|
+
+// Boolean          can_snake_pass
+// Boolean          fast_speed_snake
+
+// int[][]          food_loc
+// int[][]          speed_loc
+// int[][]          pass_loc
+
+// Boolean          speed_on_game
+// Boolean          pass_on_game
+
+void reset(){                   // Clear Game and make new snake
+
+}
+
+void set_direction(){           // Get input and set Direction 
+
+}
+
+void create_random_food(){     // generate random food on empty location
+
+}
+
+void 
